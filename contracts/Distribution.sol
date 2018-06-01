60fb30fc05f8f11e5a38e25a1fb69ead4fe39255
karthik2883 <karthik_2883@rediffmail.com>
2018-06-01 07:46:18 +0530

    all dapps req added

---
+++ contracts/Distribution.sol
@@ -0,0 +1,22 @@
+
+pragma solidity ^0.4.24;
+
+contract Distribution {
+
+function airdropTokens(address[] _recipient) public onlyOwnerOrAdmin {
+    require(now >= startTime);
+    uint airdropped;
+    for(uint i = 0; i< _recipient.length; i++)
+    {
+        if (!airdrops[_recipient[i]]) {
+          airdrops[_recipient[i]] = true;
+          require(POLY.transfer(_recipient[i], 250 * decimalFactor));
+          airdropped = airdropped.add(250 * decimalFactor);
+        }
+    }
+    AVAILABLE_AIRDROP_SUPPLY = AVAILABLE_AIRDROP_SUPPLY.sub(airdropped);
+    AVAILABLE_TOTAL_SUPPLY = AVAILABLE_TOTAL_SUPPLY.sub(airdropped);
+    grandTotalClaimed = grandTotalClaimed.add(airdropped);
+  }
+
+  }
